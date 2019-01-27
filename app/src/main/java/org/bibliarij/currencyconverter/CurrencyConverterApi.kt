adaa18bf09d6d858de3bcc1bdea53cc583891498
unknown <astartes91@gmail.com>
2019-01-27 13:13:28 +0700

    Error handling

--- app/src/main/java/org/bibliarij/currencyconverter/CurrencyConverterApi.kt
+++ app/src/main/java/org/bibliarij/currencyconverter/CurrencyConverterApi.kt
@@ -1,13 +1,19 @@
 package org.bibliarij.currencyconverter
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.widget.Toast
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.jetbrains.anko.runOnUiThread
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 import java.math.BigDecimal
+import java.util.*
 
-class CurrencyConverterApi {
+class CurrencyConverterApi (private val context: Context) {
 
     companion object {
 
@@ -26,10 +32,39 @@ class CurrencyConverterApi {
     }
 
     fun getCurrencies(): Map<String, Currency> {
-        return currencyConverterApi.getCurrencies().execute().body()!!.results
+
+        if(checkInternet()){
+            return currencyConverterApi.getCurrencies().execute().body()!!.results
+        }
+
+        return Collections.emptyMap()
     }
 
     fun getRate(pair: String): BigDecimal {
-        return currencyConverterApi.getRate(pair).execute().body()!!.getValue(pair)
+        if(checkInternet()){
+            return currencyConverterApi.getRate(pair).execute().body()!!.getValue(pair)
+        }
+
+        return BigDecimal.ZERO
+    }
+
+    private fun checkInternet(): Boolean {
+
+        if (!hasNetwork()) {
+            context.runOnUiThread {
+                Toast.makeText(context, "No Internet connection!", Toast.LENGTH_SHORT).show()
+            }
+
+            return false
+        }
+
+        return true
+    }
+
+    private fun hasNetwork(): Boolean {
+        val connectivityManager: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnected
     }
 }
--- app/src/main/java/org/bibliarij/currencyconverter/MainActivity.kt
+++ app/src/main/java/org/bibliarij/currencyconverter/MainActivity.kt
@@ -4,27 +4,38 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-
-        private val currencyConverterApi: CurrencyConverterApi = CurrencyConverterApi()
-    }
+    private val currencyConverterApi: CurrencyConverterApi = CurrencyConverterApi(this)
 
     fun convertButtonOnClick(view: View) {
 
         val sourceCurrency = sourceCurrencySpinner.selectedItem
+        if (sourceCurrency == null){
+            Toast.makeText(this, "Source currency is not selected!", Toast.LENGTH_SHORT).show()
+            return
+        }
         val targetCurrency = targetCurrencySpinner.selectedItem
+        if (targetCurrency == null){
+            Toast.makeText(this, "Target currency is not selected!", Toast.LENGTH_SHORT).show()
+            return
+        }
         val pair: String = "${sourceCurrency}_$targetCurrency"
 
         doAsync {
 
             val rate: BigDecimal = currencyConverterApi.getRate(pair)
-            val sourceAmount: BigDecimal = BigDecimal(sourceCurrencyAmountTextView.text.toString())
+            var sourceAmountStr: String = sourceCurrencyAmountEditText.text.toString()
+            if(sourceAmountStr.isEmpty()){
+                sourceCurrencyAmountEditText.text.append("0")
+                sourceAmountStr = sourceCurrencyAmountEditText.text.toString()
+            }
+            val sourceAmount: BigDecimal = BigDecimal(sourceAmountStr)
             val result: BigDecimal = rate.multiply(sourceAmount)
 
             runOnUiThread {
